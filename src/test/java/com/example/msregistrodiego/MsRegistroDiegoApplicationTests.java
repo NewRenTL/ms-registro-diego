84a15697658874edd93bc40254a39f8cf36082df
NewRenTL <diego.bustamante@utec.edu.pe>
2024-02-14 10:18:36 -0500

    added modules: Domain and Application

---
+++ src/test/java/com/example/msregistrodiego/MsRegistroDiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msregistrodiego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistroDiegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
